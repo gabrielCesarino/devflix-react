284305b794bc8c4a4a67bf9e8175d18400e18ec2
gabrielCesarino <gahnrq@hotmail.com>
2020-07-28 18:08:45 -0300

    fix carousel using react-slick

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Home from './pages/home';
 import { BrowserRouter, Switch, Route} 
 from 'react-router-dom'
 import CadastroVideo from './pages/cadastro/Video'
-
+import CadastroCategoria from './pages/cadastro/Categoria'
 const Pagina404 = () => (<div> 404 </div>)
 
 ReactDOM.render(
@@ -13,6 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component={Home} exact/>
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component = {Pagina404} />
     </Switch>
   </BrowserRouter>,
