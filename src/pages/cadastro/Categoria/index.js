a28c28a12a1954e110a1a6957de68c5c17f29c03
gabrielCesarino <gahnrq@hotmail.com>
2020-07-28 18:30:00 -0300

    starting form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,8 +6,15 @@ function CadastroCategoria(){
     return(
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
-        <Link to='/'>
-          Home
+        <form>
+          <label>
+            Nome da Categoria:
+            <input type="text" />
+          </label>
+          <button>Cadastrar</button>
+        </form>
+        <Link to="/">
+          Ir para home
         </Link>
       </PageDefault>
     )
