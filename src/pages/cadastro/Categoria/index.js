3bae2670e5cbd662a96ab671fac55dfc88748641
gabrielCesarino <gahnrq@hotmail.com>
2020-07-29 22:45:09 -0300

    using useState on form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from "../../../components/PageDefault"
 import { Link } from 'react-router-dom'
 
 function CadastroCategoria(){
+   const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes')
+    
+
     return(
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
         <form>
+
           <label>
             Nome da Categoria:
-            <input type="text" />
+            <input type="text"
+            value={nomeDaCategoria}
+            onChange = {function onChangeHandler(e){
+              setNomeDaCategoria(e.target.value)
+            }}
+            />
           </label>
           <button>Cadastrar</button>
         </form>
